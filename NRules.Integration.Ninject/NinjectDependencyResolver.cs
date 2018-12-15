f5bad754538158232786af96e8cd17307a7ca256
Alfonso Vargas <vc.alfonso@hotmail.com>
2018-12-15 00:03:09 -0600

    Class creation NinjectDependencyResolver to implement ninject
    Gets the dependencies when executing the rule

---
+++ NRules.Integration.Ninject/NinjectDependencyResolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Ninject;
+using Ninject.Modules;
+using NRules.Extensibility;
+
+namespace NRules.Integration.Ninject
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public NinjectDependencyResolver(INinjectModule container) : this(new StandardKernel(container))
+        {
+
+        }
+
+        public NinjectDependencyResolver(IKernel kernel) => _kernel = kernel;
+
+        public object Resolve(IResolutionContext context, Type serviceType) => _kernel.Get(serviceType);
+    }
+}
