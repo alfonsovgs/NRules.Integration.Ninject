e7ce6b1f44e7ef97d6349a38d9eac8b4ec700b68
Alfonso Vargas <vc.alfonso@hotmail.com>
2018-12-15 00:00:22 -0600

    Creation of class NinjectRuleActivator to implement ninject

---
+++ NRules.Integration.Ninject/NinjectRuleActivator.cs
@@ -0,0 +1,24 @@
+﻿using Ninject;
+using Ninject.Modules;
+using NRules.Fluent;
+using NRules.Fluent.Dsl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NRules.Integration.Ninject
+{
+    public class NinjectRuleActivator : IRuleActivator
+    {
+        private readonly IKernel _kernel;
+
+        public NinjectRuleActivator(INinjectModule container) : this(new StandardKernel(container))
+        {
+
+        }
+
+        public NinjectRuleActivator(IKernel kernel) => _kernel = kernel;
+
+        public IEnumerable<Rule> Activate(Type type) => _kernel.GetAll(type).Cast<Rule>();
+    }
+}
